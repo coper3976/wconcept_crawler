bb349ab13d7d5a9f0958463ef0320f909d916da6
Choihyunwoong <71638781+coper3976@users.noreply.github.com>
2021-11-10 15:23:07 +0900

    Add files via upload

---
+++ wconcept.py
@@ -0,0 +1,28 @@
+import requests
+from bs4 import BeautifulSoup
+
+my_header = {
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36',
+    'x-requested-with': 'XMLHttpRequest',
+    'accept': 'application/json, text/javascript, */*; q=0.01'
+}
+
+print(">>> Start Parsing")
+resp = requests.get('https://www.wconcept.co.kr/Search?type=direct&kwd=%EC%A1%B0%EB%8D%98', headers=my_header)
+resp.encoding = ''
+bs = BeautifulSoup(resp.text, 'lxml')
+
+soup = bs.find('div',class_='thumbnail_list')
+divs = soup.find_all('li')
+
+product_db = dict()
+for div in divs:
+    img = div.find('div', class_='img').img['src'].strip().replace('?RS=300', '').lstrip('//')
+    title = div.find('div', class_='product ellipsis multiline').text.strip()
+    price = div.find('div', class_='price').text.strip()
+
+    productLink = 'https://www.wconcept.co.kr' + div.find('a', class_='')['href']
+    
+    product_db[title] = [title, price, img, productLink] 
+
+    print(product_db)
